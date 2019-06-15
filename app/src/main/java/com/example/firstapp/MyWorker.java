d1df7322c9aa937406d72ad527516f60df1a4706
Noordeen Malango <noordinmalango@gmail.com>
2019-06-15 15:15:54 +0200

    first commiit

---
+++ app/src/main/java/com/example/firstapp/MyWorker.java
@@ -0,0 +1,8 @@
+package com.example.firstapp;
+
+public class MyWorker {
+    // a function that doubles the value
+    public  static  int doubleTheValue(int value){
+        return  value * 2;
+    }
+}
